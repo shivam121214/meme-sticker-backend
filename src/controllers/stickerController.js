c8498c28165f97621e8275682c4a8636b51fce0c
Shivam Kumar <shivamlpu1214@gmail.com>
2026-03-02 00:55:21 +0530

    adding download logic

--- src/controllers/stickerController.js
+++ src/controllers/stickerController.js
@@ -167,6 +167,7 @@ exports.downloadSticker = async (req, res) => {
     })
 
   } catch (error) {
-    return res.status(500).json({ message: "Server error" })
-  }
+  console.error("Download Error:", error)
+  return res.status(500).json({ message: error.message })
+}
 }
